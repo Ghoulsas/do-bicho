db70284ea3f4a4b837cdbbd262031c313956b053
Sergio Mendes <sergio.akirasm@gmail.com>
2021-08-08 15:46:22 -0300

    feat: "adicionado http adaptador basico"

---
+++ src/sites-adapters/http-handlers/basic-http-adapter.js
@@ -0,0 +1,13 @@
+class BasicHttpHandler {
+   _option = {
+        timeout: 5000,
+        noRetry :true,
+        resolveBodyOnly: true,
+        responseType: 'buffer'
+   }
+   async paginaResultadoDoDia(dia, options){
+
+   }
+}
+
+module.exports = BasicHttpHandler;
